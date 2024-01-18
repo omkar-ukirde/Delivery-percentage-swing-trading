a0f8f04a21ca7db2ce25770d36ec98fec330f51a
omkar ukirde <10123675+omkar-ukirde@users.noreply.github.com>
2024-01-18 08:43:14 +0530

    Update python-dailybhav.py

--- python-dailybhav.py
+++ python-dailybhav.py
@@ -1,4 +1,4 @@
-cat *.csv > alldata.csv
+#cat *.csv > alldata.csv
 import pandas as pd
 df = pd.read_csv('/Users/omkar/Downloads/Bhavcopy/alldata.csv')
 max_delivery = df.loc[df.groupby('SYMBOL')[' DELIV_PER'].transform(max) == df[' DELIV_PER']]
