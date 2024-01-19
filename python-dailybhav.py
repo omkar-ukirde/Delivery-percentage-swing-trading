3a6a8ad4497e1113b5a62c0a210556b51746e7d0
omkar ukirde <10123675+omkar-ukirde@users.noreply.github.com>
2024-01-19 07:27:27 +0530

    Update python-dailybhav.py

--- python-dailybhav.py
+++ python-dailybhav.py
@@ -1,8 +1,12 @@
+#Get 20 days data. Check for duplicate data.
 #cat *.csv > alldata.csv
 import pandas as pd
 df = pd.read_csv('/Users/omkar/Downloads/Bhavcopy/alldata.csv')
-max_delivery = df.loc[df.groupby('SYMBOL')[' DELIV_PER'].transform(max) == df[' DELIV_PER']]
-max_high = df.loc[df.groupby('SYMBOL')[' HIGH_PRICE'].transform(max) == df[' HIGH_PRICE']]
-combinedf = pd.concat([max_high,max_delivery])
-combinedf.drop_duplicates()
-combinedf.to_csv('final.csv')
+max_deliv = df.loc[df.groupby('SYMBOL')[' DELIV_PER'].transform(max) == df[' DELIV_PER']]
+max_deliv.to_csv('final.csv')
+# open in excel
+# Filter on Date for highest date
+# Filter on series for EQ.
+# copy this data to google sheets
+# get marketcap and remove data having no market cap
+# copy to excel and =close - open. Filter for positive values.  
